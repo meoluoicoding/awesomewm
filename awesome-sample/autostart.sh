9dcfe085c69e36745ed994998ffc0c7eab9bb723
meoluoicoding <meoluoicoding@gmail.com>
2022-08-02 10:06:05 +0700

    push code

---
+++ awesome-sample/autostart.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+run nm-applet
+run nm-applet
+run xfce4-power-manager
+run blueberry-tray
+run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+run numlockx on
+run volumeicon
+run nitrogen --restore
